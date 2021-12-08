23ddc8ff00bcd37f44624b40553bf70374c97ea3
framove <framove@hotmail.com>
2021-12-08 19:17:49 +0100

    iteration1.js

---
+++ iteration1.js
@@ -0,0 +1,16 @@
+
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        return numberOne;
+        }else {
+        return numberTwo;
+    } 
+  }
+
+let result = sum(5,2);
+console.log("El número más alto es " + result);
+
+
+
+
+
